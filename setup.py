55533317ccd33cc8d792f07e49de04fd9fca1fb4
hoto17296 <mail@hoto.me>
2017-08-25 20:11:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='starmie',
     version='1.0',
     description='A* algorithm implementation',
+    author='hoto17296',
+    license='MIT',
     url='https://github.com/hoto17296/starmie',
     py_modules=['starmie'],
 )
